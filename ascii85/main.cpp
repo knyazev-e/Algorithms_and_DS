f8f34b449884287bc4ab0c45a76bb2900f496327
vboxuser <vboxuser@Uninstall.myguest.virtualbox.org>
2025-03-23 09:15:06 +0000

    finished the encoder and the decoder functions

--- ascii85/main.cpp
+++ ascii85/main.cpp
@@ -1,18 +1,26 @@
-#include <vector>
 #include <iostream>
 #include <string>
+#include <cmath>
+
 
 const std::string alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstu";
 
-std::string encode(uint8_t (&chunk)[4]) {
+
+std::string encode(std::string ascii_chunk) {
     std::string encoded = "";
+    uint8_t chunk[4];
+
+    for (int i = 0; i < 4; ++i) {
+        chunk[i] = static_cast<uint8_t>(ascii_chunk[i]);
+    }
+
     uint32_t value = (chunk[0] << 24) | (chunk[1] << 16) | (chunk[2] << 8) | chunk[3];
 
     if (value == 0) {
         encoded = "z";
     }
 
-    while (vlaue > 0) {
+    while (value > 0) {
         encoded = alphabet[value % 85] + encoded;
         value = value / 85;
     }
@@ -20,9 +28,24 @@ std::string encode(uint8_t (&chunk)[4]) {
     return encoded;
 }
 
-uint8_t chunk[4] decode(std::string ascii_code) {
 
+std::string decode(std::string ascii85_chunk) {
+    std::string decoded = "";
+    uint8_t chunk[4];
+    uint32_t value = pow(85, 4)*alphabet.find(ascii85_chunk[0]) + pow(85, 3)*alphabet.find(ascii85_chunk[1]) + pow(85, 2)*alphabet.find(ascii85_chunk[2]) + 85*alphabet.find(ascii85_chunk[3]) + alphabet.find(ascii85_chunk[4]);
+
+    chunk[0] = (value >> 24) & 0xFF;
+    chunk[1] = (value >> 16) & 0xFF;
+    chunk[2] = (value >> 8) & 0xFF;
+    chunk[3] = (value >> 0) & 0xFF;
+
+    for (uint8_t byte : chunk) {
+        decoded += static_cast<char>(byte);
+    }
+
+    return decoded;
 }
+
 
 int main(int argc, char* argv[]) {
     // encoding/decoding mode
@@ -35,33 +58,34 @@ int main(int argc, char* argv[]) {
     }
 
     // reading the input
+    std::string output = "";
     std::string input;
     char byte;
-
     while(std::cin.get(byte)) {
         input.push_back(byte);
     }
 
-    // calling main functions
-    std::string output = "";
-
+    // decoding/encoding process
     if (encoding_mode) {
-        // call the encoding func
         size_t padding = (4 - (input.length() % 4)) % 4;
-        input.append(padding, "\0");
+        input.append(padding, '\0');
 
         for (int i = 0; i < input.length(); i+=4) {
-            uint8_t chunk[4];
-            for (int j = 0; j < 4; ++j) {
-                chunk[j] = static_cast<uint8_t>(input[i + j]);
-            }
-            output += encode(&chunk[4]);
+            output += encode(input.substr(i, 4));
         }
         output.erase(output.length() - padding, padding);
     }
 
     else {
-        //call the decoding func
+        size_t padding = (5 - (input.length() % 5)) % 5;
+        input.append(padding, 'u');
 
+        for (int i; i < input.length(); i+=5) {
+            output += decode(input.substr(i, 5));
+        }
+        output.erase(output.length() - padding, padding);
     }
+
+    std::cout << output << std::endl;
+    std::cout << "the code finished executing" << std::endl;
 }
